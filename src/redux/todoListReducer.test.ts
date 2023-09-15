4971ec3bb6ee6559a7a47e84583219ef47050728
De-Lynn <k.riz98@mail.ru>
2023-09-15 15:52:57 +0300

    gh-pages

--- src/redux/todoListReducer.test.ts
+++ src/redux/todoListReducer.test.ts
@@ -1,5 +1,5 @@
 import { v1 } from "uuid";
-import { todoListReducer, addTask} from "./todoListReducer";
+import { todoListReducer, addTask, clearCompletedTasks} from "./todoListReducer";
 
 it('the number of tasks should increase by 1', () => {
     // 1. data
@@ -16,4 +16,21 @@ it('the number of tasks should increase by 1', () => {
 
     // 3. expectation
     expect(newState.todos.length).toBe(3)
+})
+
+it('the number of completed tasks should be equal to 0', () => {
+    // 1. data
+    let action = clearCompletedTasks()
+    let state = {
+        todos: [
+            {id: v1(), title: 'Do components.', isDone: false},
+            {id: v1(), title: 'Do styles.', isDone: false},
+        ]
+    }
+
+    // 2. action
+    let newState = todoListReducer(state, action)
+
+    // 3. expectation
+    expect(newState.completedTasks.length).toBe(0)
 })
